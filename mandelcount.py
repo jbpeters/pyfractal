9d72815024425e52fd2082ade558341d7d71551d
Jeff Peters <jeffp@swva.net>
2017-01-28 11:34:57 -0500

    update

--- mandelcount.py
+++ mandelcount.py
@@ -1,10 +1,10 @@
-
+power=2
 def z_count(z, dist, Max_iter ):
     count=0
     zz=complex(z)
     z=complex(0,0)
     while(count < Max_iter and abs(z)< dist):
-        nz=(z*z + zz)
+        nz=(z**power + zz)
         z=nz
         count+=1
     return(count)
